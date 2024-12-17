3f2c9378daedf8487e0a8f1a94c50f72996012d1
hanezu <13895824+hanezu@users.noreply.github.com>
2024-12-16 22:30:39 -0500

    wip before back to japan

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -17,24 +17,26 @@ export default function Footer(props) {
 
   return (
     <footer className='relative w-full dark:bg-black px-6 border-t'>
-      <DarkModeButton className='text-center pt-4' />
+      <div className='container mx-auto max-w-4xl py-6 flex justify-between items-center text-sm'>
+        <DarkModeButton className='text-center' />
 
-      <div className='dark:text-yellow-300 container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
-        <div className='text-center'>
-          &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
-          reserved.
-        </div>
-        <div className='md:p-0 text-center md:text-right text-xs'>
-          {/* 右侧链接 */}
-          {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
-          {siteConfig('BEI_AN') && (
-            <a
-              href='https://beian.miit.gov.cn/'
-              className='text-black dark:text-gray-200 no-underline hover:underline ml-4'>
-              {siteConfig('BEI_AN')}
-            </a>
-          )}
-          <BeiAnGongAn />
+        <div className='dark:text-yellow-300 flex flex-wrap md:flex-no-wrap justify-between items-center w-full'>
+          <div className='text-center'>
+            &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
+            reserved.
+          </div>
+          <div className='md:p-0 text-center md:text-right text-xs'>
+            {/* 右侧链接 */}
+            {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+            {siteConfig('BEI_AN') && (
+              <a
+                href='https://beian.miit.gov.cn/'
+                className='text-black dark:text-gray-200 no-underline hover:underline ml-4'>
+                {siteConfig('BEI_AN')}
+              </a>
+            )}
+            <BeiAnGongAn />
+          </div>
         </div>
       </div>
     </footer>
